c5ff5a1a0fd561bc5858f26878e52011a847dcb8
Chaim-Rosentaler <156324688+Chaim-Rosentaler@users.noreply.github.com>
2024-09-30 11:56:33 +0300

    Update MenuPage.jsx

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -3,45 +3,45 @@ import { Link } from 'react-router-dom';
 
 const MenuPage = () => {
   return (
-    <div className="min-h-screen flex flex-wrap items-start justify-evenly p-6 bg-gray-900 ">
+    <div className="min-h-screen flex flex-wrap items-start justify-center p-4 gap-y-2">
       <Link
         to="/alfon"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-16 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         אלפון
       </Link>
       <Link
         to="/campains"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-16 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         קמפיינים
       </Link>
       <Link
         to="/commitment"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl h-40 w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-14 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all h-40 w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         התחייבויות ותשלומים
       </Link>
       <Link
         to="/reports"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-16 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         דוחות
       </Link>
       <Link
         to="/memorial-board"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-16 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         לוח הנצחה
       </Link>
       <Link
         to="/petty-cash"
-        className="bg-gray-800 text-white text-center p-16 mx-3 rounded-lg shadow-lg transform transition-transform hover:scale-105 hover:bg-gray-700 hover:shadow-2xl w-full md:w-1/3 lg:w-1/4 text-2xl font-semibold tracking-wide border border-gray-700 hover:border-gray-500"
+        className="bg-blue-200 text-blue-900 text-center p-16 mx-2 rounded-lg shadow-md hover:bg-blue-300 transition-all w-full md:w-1/3 lg:w-1/4 text-2xl"
       >
         קופה קטנה
       </Link>
     </div>
   );
-  };
+};
 
 export default MenuPage;
