1965468c511e669b80546e2cd360bc315912d819
shlomohalevi <shlomoww@gmail.com>
2024-12-25 14:24:23 +0200

    update

--- src/pages/ReportNavigation.jsx
+++ src/pages/ReportNavigation.jsx
@@ -8,19 +8,19 @@ function ReportNavigation() {
     <div className='flex flex-col grow justify-center items-center bg-indigo-100 '>
 
         <section className='flex justify-center items-center gap-4 h-[100%]'>
-            <button className='bg-indigo-500 p-2 w-[100px]  rounded-sm text-white text-center align-middle'
+            <button className='bg-indigo-500 p-2 w-[100px] h-[100px]  rounded-sm text-white text-center align-middle'
             onClick={() => navigate('/report-commitments')}
             >
                 דו"ח התחייבויות
             </button>
-            <button className='bg-indigo-500 p-2 w-[100px]  rounded-sm text-white text-center align-middle'
+            <button className='bg-indigo-500 p-2 w-[100px] h-[100px]  rounded-sm text-white text-center align-middle'
             onClick={() => navigate(`/report-payments?filter=campainName`)}
 
             
             >
                דו"ח תשלומים לפי קמפיין
             </button>
-            <button className='bg-indigo-500 p-2 w-[100px]  rounded-sm text-white text-center align-middle'
+            <button className='bg-indigo-500 p-2 w-[100px] h-[100px]  rounded-sm text-white text-center align-middle'
             onClick={() => navigate(`/report-payments?filter=dateRange`)}
             
             >
