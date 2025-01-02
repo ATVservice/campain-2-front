594042e89f990af97e3a8b027c6850c99d614e8c
Chaim-Rosentaler <r0527135949@gmail.com>
2025-01-02 10:56:33 +0200

    02/01 10:56

--- src/components/DynamicTitle.jsx
+++ src/components/DynamicTitle.jsx
@@ -9,20 +9,30 @@ const DynamicTitle = () => {
     const titles = {
       "/": "כניסה",
       "/login": "התחברות",
+      "/user-profile": "אזור אישי",
       "/forgot-password": "שחזור סיסמה",
       "/reset-password": "איפוס סיסמה",
       "/alfon": "אלפון",
+      "/add-person": "הוספת תורם",
       "/menu": "תפריט",
       "/commitments": "התחייבויות",
+      "/commitment-details": "פרטי התחייבות",
+      "/payments-without-commitment": "תשלומים ללא התחייבות",
       "/user-details": "פרטי משתמש",
       "/campains": "קמפיינים",
-      "/memorial-Board": "לוח זיכרון",
+      "/campain": "ניהול קמפיין",
+      "/edit-campain": "עריכת פרטי קמפיין",
+      "/peopleincampain": "תורמים בקמפיין",
+      "/memorial-board": "לוח הנצחה",
       "/petty-cash": "קופה קטנה",
+      "/report-navigation": "דוחות",
+      "/report-commitments": "דוח התחייבויות",
+      "/report-payments": "דוח תשלומים",
     };
 
     // מציאת כותרת מתאימה או ברירת מחדל
     const path = location.pathname.split("/")[1];
-    const title = titles[`/${path}`] || "ברוך הבא";
+    const title = titles[`/${path}`] || "ישיבת סאטמר";
     document.title = title;
   }, [location]);
 
